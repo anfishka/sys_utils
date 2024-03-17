a5b1c458fed501b488ecaf3ad650a84ce75666ee
Hanna Borodina <venera131916@gmail.com>
2024-03-18 00:26:59 +0200

    complete task

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
 ﻿//1) Написать утилиту для множесвенного открытия вкладок гугл хром, пользотель вводит число сколько их отрыть, утилита итерированно их порождает 
+using System.Diagnostics;
+
+int amountOfBlanks = int.Parse(Console.ReadLine());
+string configs = "--start-fullscreen";
+int i = 1;
+
+
+while (i <= amountOfBlanks)
+{
+    string frameUrl = $"https://www.google.ru/search?q=число{i}";
+
+    Process.Start("\"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe\"", $"{configs} {frameUrl}");
+    i++;
+}
